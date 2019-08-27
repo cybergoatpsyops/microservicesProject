1a44920959e4a084c5a934b4f4e9ab10f2b4a8a9
cybergoatpsyops <leo@reconinfosec.com>
2019-08-27 14:42:28 -0500

    add output to kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=cybergoatpsyops/projectdemo
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run projectdemo --image=cybergoatpsyops/projectdemo:latest  --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployments/projectdemo --type="NodePort" --port 8080
 
